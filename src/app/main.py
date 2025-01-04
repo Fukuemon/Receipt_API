3960de8eeb7ede45dba6eb2da7fe7a2f2ae498f1
Fukuemon <110335987+Fukuemon@users.noreply.github.com>
2025-01-04 11:34:22 +0900

    Revert "開発環境の構築"

--- src/app/main.py
+++
@@ -1,9 +0,0 @@
-from config.config import Settings
-from mangum import Mangum
-from server.api import APIApplication
-
-settings = Settings()
-
-app = APIApplication(settings).get_app()
-
-handler = Mangum(app)
--- src/app/server/route/health_route.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-
-@router.get("/health_check")
-async def handle_health_check() -> dict:
-    return {"status": "ok"}
